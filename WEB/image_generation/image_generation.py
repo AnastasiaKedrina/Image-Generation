7901c1c5bf1251651b8c4b5e427437b34090b68e
AnastasiaKedrina <113825953+AnastasiaKedrina@users.noreply.github.com>
2024-07-03 19:30:29 +0800

    Update image_generation.py

--- WEB/image_generation/image_generation.py
+++ WEB/image_generation/image_generation.py
@@ -1,26 +1,129 @@
 import requests
 import datetime
-from PIL import Image
-import sys
 import json
+import sys
+from PIL import Image
+from rembg import remove
+
+from monster_api import SD_background_generation, SD_transparent_object_generation
+from template import create_square_template_1, create_square_template_2
+from template import create_portrait_template_1
+from template import create_landscape_template_1
 
 
 def url_to_img_file(img_url):
-  folder_name = 'image_generation/result_templates'
+  folder_name = 'image_generation/rest_images'
   uniq_filename = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '-')
   result_image = Image.open(requests.get(img_url, stream=True).raw)
   result_image.save(f"{folder_name}/all/{uniq_filename}.png")
-  result_image.save(f"{folder_name}/result.png")
+
+
+# def url_to_img_file(img_url, filename='uniq', filename_prefix=''):
+#   if filename == 'uniq':
+#     img_filename = str(datetime.datetime.now().date()) + '_' + str(datetime.datetime.now().time()).replace(':', '-')
+#     if filename_prefix and not filename_prefix.isspace():
+#         img_filename = f'{filename_prefix}___{img_filename}'
+#     result_image = Image.open(requests.get(img_url, stream=True).raw)
+#     result_image.save(f"rest_images/{img_filename}.png")
+#   else:
+#     img_filename = filename
+#     result_image = Image.open(requests.get(img_url, stream=True).raw)
+#     result_image.save(f"{img_filename}.png")
   
-# if __name__ == "__main__":
-#     import sys
-#     url_to_img_file(sys.argv[1])
-#     # url_to_img_file('https://processed-model-result.s3.us-east-2.amazonaws.com/a6d21138-ec38-4d7a-8bd0-8fcfbf304f61_0.png')
+
+def get_promt_style(input_style):
+    styles_file = open("image_generation/generation_styles.csv", "r").readlines()
+    for style in styles_file[1:]:
+        s = style.split(',\"')
+        if len(s)==2: s.append('')
+        stylename, promt, negative_promt = s
+        promt = promt.replace('\"', '')
+        negative_promt = negative_promt.replace('\"', '')  
+        if input_style in stylename:
+            return promt, negative_promt
+
+def remove_background(file_name):
+    input = Image.open(f'image_generation/{file_name}.png')
+    remove(input).save(f'image_generation/{file_name}_transparent.png')
+    print(f'Изображение {file_name}_transparent.png сохранено')
+
+def generate_and_save_image(img_type, object_promt, aspect_ratio, file_path, style, color):
+  style_promt, style_negative_promt = style
+  colors_promt, colors_negative_promt = color
+
+  if img_type=='back':
+    input_promt = f'room designed for {object_promt}, all the objects in the room are positioned correctly and stylized, ' + style_promt + colors_promt
+    background_img_url = SD_background_generation(input_promt, style_negative_promt,  aspect_ratio=aspect_ratio) #colors_negative_promt
+    print(f'Сгенерировано изображение для фона: {background_img_url}')
+    url_to_img_file(background_img_url)#, filename_prefix=style_name)
+    url_to_img_file(background_img_url, file_path)
+    return file_path
+  else:
+    obj_img_url = SD_transparent_object_generation(object_promt)#, style_negative_promt)
+    print(f'Сгенерировано изображение на белом фоне:{obj_img_url}')
+    url_to_img_file(obj_img_url)#, style_name)
+    url_to_img_file(obj_img_url, file_path)#, style_name)
+    remove_background(file_path)
+    return file_path
+
+
 
 
 if __name__ == "__main__":
-    # Expecting to receive JSON data as input
-    input_data = json.load(sys.stdin)
-    description = input_data.get("description")
-    size = input_data.get("size")
-    url_to_img_file(description)
+  fonts = {
+      'helvetica': "image_generation/fonts/helvetica_regular.otf"
+      }
+  colors = {
+      # RGBA
+      # используются для текста, расчитано только на белый и черный, так как убирается фон
+      'white' : (255, 255, 255, 255),
+      'black' : (0, 0, 0, 255)
+  }
+
+  aspect_ratios = {'square': {'size':(1024, 1024), 'funcs':[create_square_template_1, create_square_template_2]},
+                        'portrait': {'size':(768, 1024), 'funcs':[create_portrait_template_1]},
+                        'landscape': {'size':(1024, 768), 'funcs':[create_landscape_template_1]}}
+
+
+
+  style_name = 'PHOTOGRAPHY'
+  # object_promt = 'gaming laptop, open, led keyboard, led lighting around the screen, gaming computers and peripherals'
+  object_promt = 'computer gaming mouse, led light, led mouse buttons'
+
+  style_promt, style_negative_promt = get_promt_style(style_name)
+  colors_promt = 'dark, contrasting, decorative smoke in the air, contrasting, cyberpunk, illuminated by neon light'
+  style = [style_promt, style_negative_promt] 
+  color = [colors_promt, '']
+
+
+  obj_file_path = 'image_generation/generated_images/obj'
+  # --генерация изображений
+  # back_file_path = generate_and_save_image('back' , object_promt, aspect_ratio, back_file_path, style, color)
+  # obj_file_path = generate_and_save_image('object' , object_promt, aspect_ratio, obj_file_path, style, color)
+
+  obj_file_path = obj_file_path+'_transparent'
+
+ 
+  # --создание шаблона
+  # aspect_ratio = list(aspect_ratios.keys())[0] # ----------- соотношение сторон, 0 - square, 1 - portrait, 2 - landscape
+  # вызов функций create_*_template, в зависимости от выбранного соотношения сторон
+  for aspect_ratio in aspect_ratios.keys(): 
+    img_size = aspect_ratios[aspect_ratio]['size']
+    back_file_path = f'image_generation/generated_images/{aspect_ratio}_back'
+
+    template_num = 1 # номер шаблона ( 1 - create_square_template_1, 2 - create_square_template_2
+    template_func = aspect_ratios[aspect_ratio]['funcs'][template_num-1]
+    
+    locals()[aspect_ratios[aspect_ratio][template_func(back_file_path, obj_file_path, img_size, fonts, colors)]]
+
+
+  # тот же вызов функций
+  # create_square_template_1('image_generation/generated_images/square_back', obj_file_path, (1024, 1024), fonts, colors)
+  # create_portrait_template_1('image_generation/generated_images/portrait_back', obj_file_path, (768, 1024), fonts, colors)
+  # create_landscape_template_1('image_generation/generated_images/landscape_back', obj_file_path, (1024, 768), fonts, colors)
+
+
+  # input_data = json.load(sys.stdin)
+  # description = input_data.get("description")
+  # size = input_data.get("size")
+  # url_to_img_file(description)
